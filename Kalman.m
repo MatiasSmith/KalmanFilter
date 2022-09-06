f38f378e1c95dfc3c6a8f971bf5e7900ae84b7ef
Matias Smith <57701260+MatiasSmith@users.noreply.github.com>
2022-09-06 12:18:35 -0500

    Update Kalman.m
    Cleaned up Code, added comments

--- Kalman.m
+++ Kalman.m
@@ -7,7 +7,8 @@ warning('off',  'all')
 mkdir('./Figures')
 warning('on',  'all')
 
-T = 0.1;   %Sampling Period
+%Parameters
+T = 0.1;
 R0 = 0.01;
 Rc = 0.015;  
 Ccap = 2400;
@@ -16,7 +17,6 @@ Cbat = 18000;
 
 % System information
 Qk = [2.5E-7,0;0,0];
-% Qk = (0.0005, 0; 0,0];
 Rk = 1E-4;
 
 length = 100;
@@ -32,19 +32,13 @@ r3(1) = 2;    %initial est error
 Est = [74, 0, 0, 0, 0];
 errEst = [4, 0, 0, 0, 0];
 Mea = r1;
-%Mea(49) = Mea(49)-50;
-%Mea(50) = Mea(50)-50;
-%Mea(51) = Mea(51)-50;
 errMea = 4.* ones(length, 1);
 
 V = [0.3, 0.6, 0.5, 0.4, 0.5, 0.3, 0.6, 0.4]
 t = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
 t = [0:length-1];
 
-%for i = 2:n        % for (int ii = 2; ii < n; i++)  Declaration on line 319
-%    [xhatEKF(:, ii), Pekf(:, :, ii)] = ekf(t(ii-1), t(ii), xhatEKF(:, ii-1), ...
-%        Pekf(:, :, ii-1), ufn, V(ii)-Voc0, ff, hc, Ffn, Hfn, Qk, Rk);
-%n = length(Mea);
+%Driving loop
 n = length;
 for i = 2:n
      [Est(i), errEst(i)] = ekf(errEst(i-1), errMea(i), Est(i-1), Mea(i));
@@ -52,35 +46,33 @@ end
 
 
 constant = TrueVal.*ones(length, 1).*r5'
-%constant(49) = constant(49)-50;
-%constant(50) = constant(50)-50;
-%constant(51) = constant(51)-50;
+
 %Plotting
 figure
 plot(t, constant, 'DisplayName', "Actual SOC")
 hold on
 plot(t, Est, 'DisplayName', "Extended Kalman Filter")
-%plot(t, SOCdr, 'DisplayName', "Open Loop")
+
 ylim = ([72-5, 72+5]);
 xlim([0, length+2])
 legend
 title(["SOC Estimate Using Extended Kalman Filter, Open Loop Estimate and", "Actual SOC vs. Time"])
 
-% Page 14
 xlabel("time (s)")
 ylabel("SOC")
 saveas(gcf, "./Figures/2ekf.jpg")
 
+%Calculation
 function [nextEst, nextErrEst] = ekf(errEst, errMea, prevEst, Mea)
 
+    %Kalman Gain
     KG = errEst / (errEst + errMea);
-
+    
     nextEst = prevEst + KG*(Mea - prevEst);
 
- %   ErrEst = (errMea * errEst) / (errMea + errEst);
+    ErrEst = (errMea * errEst) / (errMea + errEst);
+    
     nextErrEst = (1-KG)*errEst;
 
 end
-%function x = f(
 
-%end
